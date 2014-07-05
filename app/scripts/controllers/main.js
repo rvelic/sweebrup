768d709b566534362751110e5590c5c38aaf98ed
Roman Velic <roman.velic@gmail.com>
2014-07-05 14:33:15 +0100

    scaffold the main view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,18 @@
  * Controller of the sweebrupApp
  */
 angular.module('sweebrupApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $modal) {
+
+    $scope.user = {};
+    
+    // Pre-fetch an external template populated with a custom scope
+    var modal = $modal({
+      scope: $scope,
+      template: 'views/modal.html',
+      animation: 'am-fade-and-scale',
+      backdrop: 'static'
+    });
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
